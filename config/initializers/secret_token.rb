12157d8969076ed037adf308f48b91d9d66beeb1
Kino Phile <mykoweb@gmail.com>
2014-06-09 17:22:56 -0700

    Initial commit for mykoweb

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DataEngineering::Application.config.secret_key_base = '1cd5b52604783fbb71b574b1af3de36d96759e323f69b60164047b10caae51ecc65651f9ff2849d93b4ab81d8042585cf2c49e3f0ffc5a197cee34309cd2fb5b'
